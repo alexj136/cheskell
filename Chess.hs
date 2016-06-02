f4cdbd1d5d204be4fcb628b77835540f82b7aff8
Alex Jeffery <apjeffery136@googlemail.com>
2016-06-02 15:13:03 +0100

    Continued movesOf implementation. Removed Maybe qualifiers for EightOf accessors, OOB indexes now raise errors.

--- Chess.hs
+++ Chess.hs
@@ -55,16 +55,16 @@ ppBoard (grid, _, _, _) =
 ----------------------------------
 
 -- Index into an EightOf
-i8 :: Int -> EightOf a -> Maybe a
-i8 0 (EightOf (c, _, _, _, _, _, _, _)) = Just c
-i8 1 (EightOf (_, c, _, _, _, _, _, _)) = Just c
-i8 2 (EightOf (_, _, c, _, _, _, _, _)) = Just c
-i8 3 (EightOf (_, _, _, c, _, _, _, _)) = Just c
-i8 4 (EightOf (_, _, _, _, c, _, _, _)) = Just c
-i8 5 (EightOf (_, _, _, _, _, c, _, _)) = Just c
-i8 6 (EightOf (_, _, _, _, _, _, c, _)) = Just c
-i8 7 (EightOf (_, _, _, _, _, _, _, c)) = Just c
-i8 _ (EightOf (_, _, _, _, _, _, _, _)) = Nothing
+i8 :: Int -> EightOf a -> a
+i8 0 (EightOf (c, _, _, _, _, _, _, _)) = c
+i8 1 (EightOf (_, c, _, _, _, _, _, _)) = c
+i8 2 (EightOf (_, _, c, _, _, _, _, _)) = c
+i8 3 (EightOf (_, _, _, c, _, _, _, _)) = c
+i8 4 (EightOf (_, _, _, _, c, _, _, _)) = c
+i8 5 (EightOf (_, _, _, _, _, c, _, _)) = c
+i8 6 (EightOf (_, _, _, _, _, _, c, _)) = c
+i8 7 (EightOf (_, _, _, _, _, _, _, c)) = c
+i8 _ _ = error "EightOf i8: Index out of bounds"
 
 eightOf :: a -> EightOf a
 eightOf a = EightOf (a, a, a, a, a, a, a, a)
@@ -72,9 +72,24 @@ eightOf a = EightOf (a, a, a, a, a, a, a, a)
 toList :: EightOf a -> [a]
 toList (EightOf (a, b, c, d, e, f, g, h)) = [a, b, c, d, e, f, g, h]
 
-fromList :: [a] -> Maybe (EightOf a)
-fromList [a, b, c, d, e, f, g, h] = Just $ EightOf (a, b, c, d, e, f, g, h)
-fromList _ = Nothing
+fromList :: [a] -> EightOf a
+fromList [a, b, c, d, e, f, g, h] = EightOf (a, b, c, d, e, f, g, h)
+fromList _ = error "EightOf fromList: length list /= 8"
+
+zipEightOf :: (a -> b -> c) -> EightOf a -> EightOf b -> EightOf c
+zipEightOf f
+    (EightOf (a1, a2, a3, a4, a5, a6, a7, a8))
+    (EightOf (b1, b2, b3, b4, b5, b6, b7, b8)) =
+        EightOf (f a1 b1, f a2 b2, f a3 b3, f a4 b4,
+            f a5 b5, f a6 b6, f a7 b7, f a8 b8)
+
+inBounds :: Location -> Bool
+inBounds (r, c) = r <= 8 && r >= 1 && c <= 8 && c >= 1
+
+addLocation :: Location -> Location -> Location
+addLocation (r1, c1) (r2, c2) = (r1 + r2, c1 + c2)
+(+~) :: Location -> Location -> Location
+(+~) = addLocation
 
 -- Label elements of an EightOf by index
 labelIdx :: EightOf a -> EightOf (Int, a)
@@ -87,15 +102,15 @@ passerIdxs :: (a -> Bool) -> EightOf a -> [Int]
 passerIdxs t = map fst . filter snd . toList . labelIdx . fmap t
 
 instance Functor EightOf where
-    fmap f = fromJust . fromList . map f . toList
+    fmap f = fromList . map f . toList
 
 -- Human-readable indexing functions
-row :: Int -> Board -> Maybe Row
+row :: Int -> Board -> Row
 row r (g, _, _, _) = i8 r g
-col :: Int -> Row -> Maybe Square
+col :: Int -> Row -> Square
 col = i8
-idx :: Location -> Board -> Maybe Square
-idx (r, c) b = row r b >>= col c
+idx :: Location -> Board -> Square
+idx (r, c) b = col c $ row r b
 
 rank :: Piece -> Rank
 rank (r, _, _) = r
@@ -103,6 +118,9 @@ clr :: Piece -> Colour
 clr (_, c, _) = c
 hasMoved :: Piece -> Bool
 hasMoved (_, _, h) = h
+
+setSquare :: Location -> Square -> Board -> Board
+setSquare l s b = undefined
 
 -- Get the locations of all White or Black Pieces on a given board
 colourLocs :: Colour -> Board -> [Location]
@@ -115,7 +133,7 @@ colourLocs colr (grid, _, _, _) =
 start :: Board
 start =
     ( EightOf
-      ( fromJust . fromList . map (\p -> Just(p, Black, False)) $
+      ( fromList . map (\p -> Just(p, Black, False)) $
           [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
       , eightOf (Just (Pawn, Black, False))
       , eightOf Nothing
@@ -123,7 +141,7 @@ start =
       , eightOf Nothing
       , eightOf Nothing
       , eightOf (Just (Pawn, White, False))
-      , fromJust . fromList . map (\p -> Just(p, White, False)) $
+      , fromList . map (\p -> Just(p, White, False)) $
           [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
       )
     , Nothing
@@ -134,7 +152,14 @@ start =
 -- Move information
 movesOf :: Location -> Board -> [(Location, Square)]
 movesOf l b = case idx l b of
-    Nothing -> []
-    Just _  -> undefined -- TODO
+    Nothing                   -> []         -- No piece, so no moves possible
+    Just (Knight, c, _)       -> undefined $ filter inBounds
+        [ (l +~ (1, 2)), (l +~ (-1, 2)), (l +~ (1, -2)), (l +~ (-1, -2))
+        , (l +~ (2, 1)), (l +~ (-2, 1)), (l +~ (2, -1)), (l +~ (-2, -1))
+        ] -- TODO
+    Just (Pawn, White, False) -> undefined  -- TODO
+    Just (Pawn, White, True ) -> undefined  -- TODO
+    Just (Pawn, Black, False) -> undefined  -- TODO
+    Just (Pawn, Black, True ) -> undefined  -- TODO
 
 --daugters :: Colour -> Board -> [Board]
